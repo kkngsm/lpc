0ea76f2fbd51301791292ad60ecb93cebd68c4f7
KOKI <kkngsm@gmail.com>
2022-09-05 23:30:35 +0900

    add freqz

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::{f32::consts::PI, fs::File};
+
+use realfft::num_complex::{Complex, ComplexFloat};
+
 macro_rules! plot_with_name {
     ($title:expr, $($name_ys:expr), +) => {
         use plotly::common::{Mode, Title};
@@ -23,35 +27,60 @@ macro_rules! plot {
     }
 }
 fn main() {
-    const LPC_ORDER: usize = 16;
+    const LPC_ORDER: usize = 32;
 
-    let original = (0..128)
-        .map(|i| {
-            let i = i as f32;
-            (i * 0.01).sin()
-                + 0.75 * (i * 0.03).sin()
-                + 0.5 * (i * 0.05).sin()
-                + 0.25 * (i * 0.11).sin()
-        })
-        .collect::<Vec<_>>();
-    // LPCで前向き予測した信号を求める
-    let mut predicted = original.clone();
-    // 過去lpcOrder分から予測するので開始インデックスはlpcOrderから
-    // それより前は予測せずにオリジナルの信号をコピーしている
+    let original: Vec<f32> = serde_json::from_str(include_str!("./wave.json")).unwrap();
+    let N = original.len();
+    let mut planner = realfft::RealFftPlanner::new();
+    let fft = planner.plan_fft_forward(N);
+    let ifft = planner.plan_fft_inverse(N);
 
-    let mut lpc = Lpc::new(LPC_ORDER);
-    let a = lpc.calc(LPC_ORDER, &original);
+    let logspec: Vec<_> = {
+        let mut spectrum = fft.make_output_vec();
+        let mut ori = original.clone();
+        fft.process(&mut ori, &mut spectrum);
 
-    for (i, p) in predicted.iter_mut().enumerate().skip(LPC_ORDER) {
-        *p = -original
-            .iter()
-            .skip(i)
-            .take(LPC_ORDER)
-            .zip(a.iter().skip(1))
-            .map(|(x, y)| x * y)
-            .sum::<f32>();
-    }
-    plot!("title", original, predicted);
+        spectrum
+            .into_iter()
+            .map(|c| c.abs().log10() * 20.0)
+            .collect()
+    };
+
+    let loglpcspec: Vec<_> = {
+        let mut lpc = Lpc::new(LPC_ORDER);
+        let lpcspec = lpc.calc(LPC_ORDER, &original);
+        lpcspec
+            .into_iter()
+            .map(|c| c.abs().log10() * 20.0)
+            .take(N / 2)
+            .collect()
+    };
+    let cepstrum: Vec<_> = {
+        let mut ori = original.clone();
+        let mut spectrum = fft.make_output_vec();
+        // cepstrum
+        fft.process(&mut ori, &mut spectrum);
+        for s in &mut spectrum {
+            s.re = s.abs().log10() * 20.0;
+            s.im = 0.0;
+        }
+        ifft.process(&mut spectrum, &mut ori);
+        for x in &mut ori {
+            *x /= N as f32;
+        }
+        let mut cepstrum = ori;
+
+        for x in cepstrum.iter_mut().take(N / 2).skip(LPC_ORDER) {
+            *x = 0.0;
+        }
+        for x in cepstrum.iter_mut().rev().take(N / 2).skip(LPC_ORDER) {
+            *x = 0.0;
+        }
+        fft.process(&mut cepstrum, &mut spectrum);
+        spectrum.into_iter().map(|c| c.re).take(N / 2).collect()
+    };
+
+    plot!("title", logspec, loglpcspec, cepstrum);
 }
 
 #[derive(Debug, Default)]
@@ -72,9 +101,11 @@ impl Lpc {
             v: Vec::with_capacity(order + 2),
         }
     }
-    pub fn calc(&mut self, order: usize, input: &[f32]) -> &[f32] {
+    pub fn calc(&mut self, order: usize, input: &[f32]) -> Vec<Complex<f32>> {
         self.auto_corr(order, input);
-        self.levinson_durbin(order)
+        self.levinson_durbin(order);
+        let e = self.e.last().unwrap().sqrt();
+        freqz(&[e], &self.coef, input.len())
     }
     fn auto_corr(&mut self, order: usize, data: &[f32]) {
         // 自己相関関数
@@ -133,4 +164,28 @@ impl Lpc {
         }
         &self.coef
     }
+}
+// 周波数応答
+fn freqz(b: &[f32], a: &[f32], n: usize) -> Vec<Complex<f32>> {
+    let mut H = vec![Complex::default(); n];
+    let n2pi = 2.0 * PI / n as f32;
+    for (i, h) in H.iter_mut().enumerate() {
+        let z = Complex::new(0.0, i as f32 * -n2pi).exp();
+        let b_len = b.len();
+        let numerator: Complex<_> = b
+            .iter()
+            .enumerate()
+            .map(|(i, x)| x * z.powi((b_len - i) as i32))
+            .sum();
+        let a_len = a.len();
+        let denominator: Complex<_> = a
+            .iter()
+            .enumerate()
+            .map(|(i, x)| x * z.powi((a_len - i) as i32))
+            .sum();
+
+        *h = numerator / denominator;
+    }
+
+    return H;
 }
